730d4142e8389d8ac0af66f7191bf6c2ebb438f0
Indudhar G P <gpindudhar@gmail.com>
2026-07-21 22:58:38 +0530

    Update website styles and fixed bugs

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://website-builder-3-dwbj.onrender.com";
 
 function Register() {
   const navigate = useNavigate();
